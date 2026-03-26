6aebe2bd3817a5bd81623f9e2c54893087b6c13b
Gabriel Soares <115197009+Zouares@users.noreply.github.com>
2026-03-25 22:21:37 -0300

    initial commit

--- __init__.py
+++ __init__.py
@@ -5,8 +5,8 @@ bl_info = {
     "blender": (4, 2, 0),
     "location": "View3D > N Panel > Modifiers",
     "description": "Add, tweak and apply modifiers in batch across multiple objects",
-    "doc_url": "https://github.com/yourname/modifier-batch-apply",
-    "tracker_url": "https://github.com/yourname/modifier-batch-apply/issues",
+    "doc_url": "https://github.com/Zouares/modifier_batch_apply",
+    "tracker_url": "https://github.com/Zouares/modifier_batch_apply/issues",
     "category": "Object",
 }
 
